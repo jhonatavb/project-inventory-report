a7dca8f216b142471728051725420eea8e4ef9dd
Daniel <danielfdoficial@gmail.com>
2022-02-01 16:33:29 -0300

    requisito 1: incompleto

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,77 @@
+# class SimpleReport:
+    # def __init__(self):
+
+from collections import Counter
+import datetime
+
+mock = [
+        {
+            "id": 1,
+            "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP",
+            "nome_da_empresa": "Forces of Nature",
+            "data_de_fabricacao": "2020-07-04",
+            "data_de_validade": "2023-02-09",
+            "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+            "instrucoes_de_armazenamento": "in blandit ultrices enim",
+        },
+        {
+            "id": 2,
+            "nome_do_produto": "sodium ferric gluconate complex",
+            "nome_da_empresa": "sanofi-aventis U.S. LLC",
+            "data_de_fabricacao": "2020-05-31",
+            "data_de_validade": "2023-01-17",
+            "numero_de_serie": "SE95 2662 8860 5529 8299 2861",
+            "instrucoes_de_armazenamento": "duis bibendum morbi",
+        },
+        {
+            "id": 3,
+            "nome_do_produto": "Dexamethasone Sodium Phosphate",
+            "nome_da_empresa": "sanofi-aventis U.S. LLC",
+            "data_de_fabricacao": "2019-09-13",
+            "data_de_validade": "2023-02-13",
+            "numero_de_serie": "BA52 2034 8595 7904 7131",
+            "instrucoes_de_armazenamento": "morbi quis tortor id",
+        },
+        {
+            "id": 4,
+            "nome_do_produto": "Uricum acidum, Benzoicum acidum",
+            "nome_da_empresa": "Newton Laboratories, Inc.",
+            "data_de_fabricacao": "2019-11-08",
+            "data_de_validade": "2019-11-25",
+            "numero_de_serie": "FR38 9203 3060 400T QQ8B HHS0 Q46",
+            "instrucoes_de_armazenamento": "velit eu est congue elementum",
+        },
+    ]
+
+
+def get_company_with_more_products(products_list):
+  company_names = []
+  company_with_more_repetitinos = ''
+  total = 0
+  for company in products_list:
+    company_names.append(company['nome_da_empresa'])
+  for company in company_names:
+    repetitions = Counter(company_names)[company]
+
+    if repetitions > total:
+      company_with_more_repetitinos = company
+      total = total + 1
+  return f'Empresa com maior quantidade de produtos estocados: {company_with_more_repetitinos}'
+
+
+def get_product_with_oldest_fabrication_date(products_list):
+  data = datetime.datetime.now()
+  fabrication_dates = []
+  for date in products_list:
+    fabrication_dates.append(date['data_de_fabricacao'])
+  oldest_date = sorted(fabrication_dates)[0]
+  return f'Data de fabricação mais antiga: {oldest_date}'
+
+get_product_with_oldest_fabrication_date(mock)
+
+def generate(products_list):
+  company_with_more_products = get_company_with_more_products(products_list)
+  oldest_fabrication_date = get_product_with_oldest_fabrication_date(products_list)
+  return f'{company_with_more_products}\n{oldest_fabrication_date}'
+        
+print(generate(mock))
