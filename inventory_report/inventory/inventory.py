888e4cf03826d1fb320c25f82391458da717838b
Jhonata <jhonatavbrg@gmail.com>
2022-02-02 09:03:39 -0300

    fix list-dict in read function

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -3,14 +3,17 @@ import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
-
 class Inventory:
     # ref https://stackoverflow.com/a/51889958
     @classmethod
     def read_file_type(cls, path, file_type):
         if path.endswith(".csv"):
-            return csv.DictReader(file_type, delimiter=",", quotechar='"')
+            reader = csv.DictReader(file_type, delimiter=",", quotechar='"')
+            csv_list = []
+            for row in reader:
+                csv_list.append(row)
 
+            return csv_list 
         if path.endswith(".json"):
             return json.load(file_type)
 
