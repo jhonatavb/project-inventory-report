d5ad310f666da5b184ec1c8a9a729a186d554127
Jhonata <jhonatavbrg@gmail.com>
2022-02-02 07:09:33 -0300

    add req 03 e 04

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,31 @@
+import csv
+import json
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    #ref https://stackoverflow.com/a/51889958
+    @classmethod
+    def read_file_type(cls, path, file_type):
+        if path.endswith(".csv"):
+            return csv.DictReader(file_type, delimiter=",", quotechar='"')
+
+        if path.endswith(".json"):
+            return json.load(file_type)
+
+    @classmethod
+    def import_data(cls, path, report_type):
+        with open(path, "r") as file_type:
+            product_list = cls.read_file_type(path, file_type)
+
+            if report_type == "simples":
+               type_report_return = SimpleReport.generate(product_list)
+
+            if report_type == "completo":
+               type_report_return = CompleteReport.generate(product_list)
+
+            if report_type != "simples" or report_type != "completo":
+                type_report_return = "Invalid format"
+
+        return type_report_return
