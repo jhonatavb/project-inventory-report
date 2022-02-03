6306ad59212668495d10f5c64704532c93e24ef0
Jhonata <jhonatavbrg@gmail.com>
2022-02-03 12:25:42 -0300

    add req 06

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,11 @@
+from inventory_report.importer.importer import Importer
+from inventory_report.inventory.inventory import Inventory
+
+
+class XmlImporter(Importer):
+    @classmethod
+    def import_data(self, path: str):
+        if path.endswith(".xml"):
+            return Inventory.read_file_type(path) 
+        else:
+            raise ValueError("Arquivo inválido")
