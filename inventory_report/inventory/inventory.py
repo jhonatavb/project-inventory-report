8633f9223918def65a837b7b1de411a73f2bd216
Jhonata <jhonatavbrg@gmail.com>
2022-02-03 10:19:36 -0300

    refactored read function

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -16,38 +16,40 @@ class Inventory:
         return my_list
 
     @classmethod
-    def read_file_type(cls, path, file_type):
-        if path.endswith(".csv"):
-            reader = csv.DictReader(file_type, delimiter=",", quotechar='"')
-            csv_list = []
-            for row in reader:
-                csv_list.append(row)
+    def read_file_type(cls, path):
+        with open(path, "r") as file_type:
+            if path.endswith(".csv"):
+                reader = csv.DictReader(
+                    file_type, delimiter=",", quotechar='"'
+                )
+                csv_list = []
+                for row in reader:
+                    csv_list.append(row)
 
-            return csv_list
+                return csv_list
 
-        if path.endswith(".json"):
-            return json.load(file_type)
+            if path.endswith(".json"):
+                return json.load(file_type)
 
-        if path.endswith(".xml"):
-            my_xml = file_type.read()
-            my_dict_xml = xmltodict.parse(my_xml)
-            my_list = cls.parse_list(my_dict_xml)
+            if path.endswith(".xml"):
+                my_xml = file_type.read()
+                my_dict_xml = xmltodict.parse(my_xml)
+                my_list = cls.parse_list(my_dict_xml)
 
-            return my_list
+                return my_list
 
     @classmethod
     def import_data(cls, path, report_type):
-        with open(path, "r") as file_type:
-            product_list = cls.read_file_type(path, file_type)
+        product_list = cls.read_file_type(path)
 
-            if report_type == "simples":
-                type_report_return = SimpleReport.generate(product_list)
-                return type_report_return
+        if report_type == "simples":
+            type_report_return = SimpleReport.generate(product_list)
+            return type_report_return
 
-            if report_type == "completo":
-                type_report_return = CompleteReport.generate(product_list)
-                return type_report_return
+        if report_type == "completo":
+            type_report_return = CompleteReport.generate(product_list)
+            return type_report_return
 
-            if report_type != "simples" or report_type != "completo":
-                type_report_return = "Invalid format"
-                return type_report_return
+        if report_type != "simples" or report_type != "completo":
+            type_report_return = "Invalid format"
+            return type_report_return
